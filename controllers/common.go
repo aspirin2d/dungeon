f104bfa49f62c6537eb72a37654448293f918961
Aspirin2D <aspirin2d@outlook.com>
2023-10-08 12:09:56 +0800

    init

---
+++ controllers/common.go
@@ -0,0 +1,14 @@
+package controllers
+
+import "github.com/gin-gonic/gin"
+
+// Ping godoc
+// @Summary		ping to the server
+// @Description	a test ping to the server
+// @Tags			test
+// @Produce		plain
+// @Success		200
+// @Router			/ping [get]
+func Ping(c *gin.Context) {
+	c.String(200, "pong")
+}
